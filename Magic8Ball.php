930df605ebea42c4e08c15c5bd9e684a4f71d255
leej6257 <50209412+leej6257@users.noreply.github.com>
2019-06-11 08:17:10 -0700

    Update Magic8Ball.php

--- Magic8Ball.php
+++ Magic8Ball.php
@@ -8,6 +8,7 @@
   <body>
 
     <h1>Magic 8 Ball</h1>
+    <p>Ask a yes or no question.</p>
 
     <?php
        // define variables and set to empty values
@@ -24,29 +25,9 @@
        }
     ?>
 
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>$
       Arg1: <input type="text" name="arg1"><br>
       <br>
       <input type="submit" value="ASK!">
     </form>
-
-    <?php
-       // only display if return code is numeric - i.e. exec has been called
-       if (is_numeric($retc)) {
-         echo "<h2>Your Input:</h2>";
-         echo $arg1;
-         echo "<br>";
-       
-         echo "<h2>Program Output (an array):</h2>";
-         foreach ($output as $line) {
-           echo $line;
-           echo "<br>";
-         }
-       
-         echo "<h2>Program Return Code:</h2>";
-         echo $retc;
-       }
-    ?>
-        
-  </body>
-</html>
+  
